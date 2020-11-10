567af84525a6b5ceb9a947137a8d1256662b4526
XDeviation <xth@LAPTOP-R1MR3AQT.localdomain>
2020-11-10 23:59:28 +0800

    add Libre2008

--- intersection_of_half_planes/intersection_of_half_planes.cpp
+++ intersection_of_half_planes/intersection_of_half_planes.cpp
@@ -1,45 +1,15 @@
-/*
- * Codeforces Contest
- * Au: Lost_Deviation
- * Time: 2020-11-09 15:15:05
- */
-#include "dbg_func"
-#include <bits/extc++.h>
 #include <bits/stdc++.h>
-/*
-若编译器没有 extc++.h 使用如下三个库
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/priority_queue.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-*/
-#define param int, less<int>, pairing_heap_tag
-using namespace std;
-// using namespace __gnu_pbds;
-#ifndef open_dbg_func
-#define dbg(args...) (args)
-#endif
 using namespace std;
 #define ll long long
-#define ull unsigned long long
-const int maxn = 1e6 + 7;
 const double eps = 1e-6;
-const double pi = acos(-1);
-const ll p = 998244353;
-const int maxp = 300010;
-static auto fast_io = []() {
-    std::ios::sync_with_stdio(false); // turn off sync
-    cin.tie(nullptr);                 // untie in/out streams
-    return 0;
-}();
-vector<string> split(string str, const string &delimiters) {
-    regex del(delimiters);
-    vector<string> v(sregex_token_iterator(str.begin(), str.end(), del, -1),
-                     sregex_token_iterator());
-    return v;
-}
+const int maxn = 1e5 + 7;
+
+// tips: this tmp need each vector has intersection
+
 bool dcmp(double a, double b) {
     return fabs(a - b) < eps;
 }
+
 class point
 // 点或向量的类
 {
@@ -51,7 +21,6 @@ class point
     void print() {
         cout << x << y;
     }
-
     point operator+(const point &p) const {
         return {x + p.x, y + p.y};
     }
@@ -76,7 +45,122 @@ class point
     double angle() {
         return atan2(y, x);
     }
-} a[maxn], tmpp[maxn], ans[maxn];
+} ans[maxn], tmpp[maxn];
+
+int pointcmp(const point &a, const point &b) {
+    if (a.x != b.x)
+        return (a.x < b.x);
+    else
+        return (a.y < b.y);
+}
+
+bool same_line(point x1, point x2, point x3)
+//判断点是否在线段上，所求点为x1，若改为直线则在if中直接return true
+{
+    const double esp = 1e-8;
+    if (abs((x3.y - x1.y) * (x2.x - x1.x) - (x2.y - x1.y) * (x3.x - x1.x)) <
+        esp) {
+        if (x1.x <= x2.x && x1.x >= x3.x && x1.y <= x2.y && x1.y >= x3.y)
+            return true;
+        if (x1.x >= x2.x && x1.x <= x3.x && x1.y >= x2.y && x1.y <= x3.y)
+            return true;
+        return false;
+    }
+    return false;
+}
+
+bool in_the_area(point p, point area[], int lens)
+//判断点是否在多边形area内部，area内的点逆时针排序，lens为多边形点的数量，same_line用来区分边界点算内部还是外部
+{
+    int ans = 0;
+    double x;
+    for (int i = 1; i <= lens; i++) {
+        point p1 = area[i];
+        point p2 = area[i + 1];
+        if (same_line(p, p1, p2)) return true;
+        if (p1.y == p2.y) continue;
+        if (p.y < min(p1.y, p2.y)) continue;
+        if (p.y >= max(p1.y, p2.y)) continue;
+        x = (p.y - p1.y) * (p2.x - p1.x) / (p2.y - p1.y) + p1.x;
+        if (x > p.x) ans++;
+    }
+    return (ans % 2 == 1);
+}
+
+ll multi(point p1, point p2, point p0)
+// 叉乘
+{
+    ll x1 = p1.x - p0.x;
+    ll y1 = p1.y - p0.y;
+
+    ll x2 = p2.x - p0.x;
+    ll y2 = p2.y - p0.y;
+
+    return (x1 * y2 - x2 * y1);
+}
+
+int ConvexHull(int n, point p[], point ch[])
+// 凸包，点p，输出ch，如果三点共线算凸包，那么把小于号改为小于等于号
+{
+    sort(p, p + n, pointcmp);
+    int m = 0;
+    for (int i = 0; i < n; i++) {
+        while (m > 1 && multi(ch[m - 1], p[i], ch[m - 2]) < 0)
+            m--;
+        ch[m++] = p[i];
+    }
+    int k = m;
+    for (int i = n - 2; i >= 0; i--) {
+        while (m > k && multi(ch[m - 1], p[i], ch[m - 2]) < 0)
+            m--;
+        ch[m++] = p[i];
+    }
+    if (n > 1) m--;
+    return m;
+}
+
+int cnt(int x, int y, int m) {
+    x = x % m;
+    y = y % m;
+    if (x <= y)
+        return y - x + 1;
+    else
+        return y + m - x + 1;
+}
+
+double Rotating_Caliper(int m, point ch[])
+// 旋转卡壳，i，j是俩点
+{
+    ch[m + 1] = ch[1];
+    int j = 2;
+    double ans = 0;
+    for (int i = 1; i <= m; i++) {
+        while (fabs(multi(ch[i], ch[i + 1], ch[j])) <
+               fabs(multi(ch[i], ch[i + 1], ch[j + 1]))) {
+            j++;
+            if (j > m) j = 1;
+        }
+        ans = max(ans, (ch[i] - ch[j]).len());
+    }
+    return ans;
+}
+
+double cross(point a, point b)
+//求向量外积
+{
+    return a.x * b.y - a.y * b.x;
+}
+
+double area(point s[], int n)
+//求凸多边形面积
+{
+    double ret = 0;
+    s[n + 1] = s[1];
+    for (int i = 1; i <= n; i++)
+        ret += cross(s[i], s[i + 1]);
+    return fabs(ret / 2);
+}
+
 struct line
 // 线
 {
@@ -91,48 +175,41 @@ struct line
     line(point a, point b) {
         this->s = a, this->t = b, this->ang = (a - b).angle();
     }
-    double len() {
-        return (s - t).len();
-    }
 } li[maxn], tmpl[maxn];
-double drgcos(line a, line b) {
-    return ((b.t - b.s) * (a.t - a.s) / a.len() * b.len());
-}
-double cross(point a, point b)
-//求向量外积
-{
-    return a.x * b.y - a.y * b.x;
-}
-bool parallel(line a, line b)
-//平行
-{
-    return dcmp(cross(a.t - a.s, b.t - b.s), 0);
-}
-bool linecmp(line a, line b)
-// 极角序
-{
-    return dcmp(a.ang, b.ang) ? cross(a.t - a.s, b.t - a.s) + eps < 0
-                              : a.ang < b.ang;
-}
-bool isrig(line a, point b)
-//判断点是否在向量右边
-{
-    return cross(a.t - a.s, b - a.s) + eps < 0;
-}
+
 point intersection(line a, line b)
 // 交点
 {
     return a.s + (a.t - a.s) * (cross(a.s - b.s, b.t - b.s) /
                                 cross(b.t - b.s, a.t - a.s));
 }
+
+bool parallel(line a, line b)
+//平行
+{
+    return dcmp(cross(a.t - a.s, b.t - b.s), 0);
+}
+
+bool isrig(line a, point b)
+//判断点是否在向量右边
+{
+    return cross(a.t - a.s, b - a.s) + eps < 0;
+}
+
+bool linecmp(line a, line b)
+// 极角序
+{
+    return dcmp(a.ang, b.ang) ? cross(a.t - a.s, b.t - a.s) + eps < 0
+                              : a.ang < b.ang;
+}
+
 bool SI(line *li, int n, point *ret, int &m, line *ql, point *qp)
 // 半平面交，li是向量集合，n是数量，ret是答案，ql和qp是临时的
 {
     int l, r;
     sort(li + 1, li + 1 + n, linecmp);
     ql[l = r = 1] = li[1];
-    for (int i = 2; i <= n; i++) {
-        dbg(i);
+    for (int i = 2; i <= n; i++)
         if (!dcmp(li[i].ang, li[i - 1].ang)) {
             while (l < r && isrig(li[i], qp[r - 1]))
                 --r;
@@ -144,8 +221,6 @@ bool SI(line *li, int n, point *ret, int &m, line *ql, point *qp)
                 (parallel(ql[l], ql[l + 1]) || parallel(ql[r], ql[r - 1])))
                 return false;
         }
-    }
-    // dbg(n);
     while (l < r && isrig(ql[l], qp[r - 1]))
         --r;
     while (l < r && isrig(ql[r], qp[l]))
@@ -157,54 +232,58 @@ bool SI(line *li, int n, point *ret, int &m, line *ql, point *qp)
         ret[++m] = qp[i];
     return true;
 }
-double solvef(double a, double b, double c) {
-    return max((-b + sqrt(b * b - 4 * a * c)) / (2 * a),
-               (-b - sqrt(b * b - 4 * a * c)) / (2 * a));
-}
-double area(point s[], int n)
-//求凸多边形面积
+
+struct Point
+// 三维
 {
-    double ret = 0;
-    s[n + 1] = s[1];
-    for (int i = 1; i <= n; i++)
-        ret += cross(s[i], s[i + 1]);
-    return fabs(ret / 2);
-}
-signed main(void) {
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        a[i].read();
+    double x, y, z;
+    void read() {
+        scanf("%lf%lf%lf", &x, &y, &z);
     }
-    line str(a[0], a[1]);
+    Point operator+(const Point &p) const {
+        return {x + p.x, y + p.y, z + p.z};
+    }
+    Point operator-(const Point &p) const {
+        return {x - p.x, y - p.y, z - p.z};
+    }
+    Point operator*(double p) const {
+        return {x * p, y * p, z * p};
+    }
+    Point operator/(double p) const {
+        return {x / p, y / p, z / p};
+    }
+    Point operator*(const Point &p) const {
+        return {y * p.z - z * p.y, -x * p.z + z * p.x, x * p.y - y * p.x};
+    }
+    double operator^(const Point &p) const {
+        return x * p.x + y * p.y + z * p.z;
+    }
+    double len() {
+        return sqrt(x * x + y * y + z * z);
+    }
+    double dis(const Point &p) const {
+        return ((*this) - p).len();
+    }
+};
+
+point a[maxn], b[maxn];
+
+int main() {
+    int t;
+    cin >> t;
     int tot = 0;
-    for (int i = 1; i < n; i++) {
-        line tmp(a[i], a[(i + 1) % n]);
-        if (parallel(str, tmp)) {
-            point inter =
-                (str.t + tmp.s) * (tmp.len()) / (tmp.len() + str.len());
-            point ender =
-                (str.s + tmp.t) * (tmp.len()) / (tmp.len() + str.len());
-            li[++tot] = line(inter, ender);
-        } else {
-            point inter = intersection(str, tmp);
-            double x = cos(pi - acos(drgcos(str, tmp))),
-                   k = tmp.len() / str.len();
-
-            double y = (1 - x * x) / (2 * x + k * k + 1);
-            // dbg(x, k, inter.x, inter.y, y);
-
-            point add;
-            add.x = 1, add.y = 1 / tan(tmp.ang + asin(sqrt(y)));
-            li[++tot] = line(inter, inter - add);
-        }
-        dbg(tot, li[tot].s.x, li[tot].s.y, li[tot].t.x, li[tot].t.y,
-            li[tot].ang);
+    while (t--) {
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+            a[i].read();
+        for (int i = 0; i < n; i++)
+            li[++tot].getline(a[i], a[(i + 1) % n]);
     }
-    int m;
-    for (int i = 1; i <= tot; i++) {
-    }
-    SI(li, tot, ans, m, tmpl, tmpp);
-    dbg(m);
-    cout << fixed << setprecision(10) << area(ans, m) / area(a, n);
+    int m = 0;
+    if (SI(li, tot, ans, m, tmpl, tmpp))
+        printf("%.3f", area(ans, m));
+    else
+        cout << "0.000";
+    return 0;
 }
